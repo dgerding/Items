f1bbf4d885de1a1404ea795775c263f46740af63
David Gerding <dave@gerding.us>
2018-04-25 11:06:49 -0500

    Add project files.

---
+++ Items/Items/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Items.Models;
+
+namespace Items.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
